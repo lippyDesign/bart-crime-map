fb45979090ca71523f73703875b92b8c90f932fc
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-09-03 14:58:32 -0700

    added animated control panel

--- src/ExampleMap.js
+++ src/ExampleMap.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import MapGL from 'react-map-gl';
-import DeckGL, {ScatterplotLayer} from 'deck.gl';
+import DeckGL, { ScatterplotLayer } from 'deck.gl';
+import { CSSTransitionGroup } from 'react-transition-group';
+import anime from 'animejs'
 
 import ControlPanel from './ControlPanel';
 import BartOverlay from './BartOverlay';
@@ -16,7 +18,7 @@ export default class App extends Component {
     viewport: {
       latitude: 37.785164,
       longitude: -122.41669,
-      zoom: 16.140440,
+      zoom: 12,
       bearing: -20.55991,
       pitch: 60,
       width: window.innerWidth,
@@ -32,7 +34,8 @@ export default class App extends Component {
       maxZoom: 20,
       minPitch: 0,
       maxPitch: 85
-    }
+    },
+    selectedStation: null
   }
 
   componentDidMount() {
@@ -60,8 +63,46 @@ export default class App extends Component {
     settings: {...this.state.settings, [name]: value}
   });
 
+  _hideControlPanel = () => this._controlPanelAnimation('close')
+
+  _controlPanelAnimation = motion => {
+    const obj = motion === 'close' ? { width: '0', height: '0', opacity: '0', right: '-370' } : { width: '340', height: '400', opacity: '1', right: '0' };
+    anime({
+      targets: document.querySelector('.control-panel'),
+      width: {
+        value: obj.width,
+        duration: 250,
+        easing: 'easeInOutSine'
+      },
+      height: {
+        value: obj.height,
+        duration: 250,
+        easing: 'easeInOutSine'
+      },
+      right: {
+        value: obj.right,
+        duration: 250,
+        easing: 'easeInOutSine'
+      },
+      opacity: {
+        value: obj.opacity,
+        duration: 250,
+        easing: 'easeInOutSine'
+      }
+    });
+  }
+
+  _renderControlPanel() {
+    const { selectedStation } = this.state;
+    return selectedStation ? <ControlPanel selectedStation={selectedStation} _hideControlPanel={this._hideControlPanel} /> : null;
+  }
+
+  _stationClicked = station => {
+    this.setState({ selectedStation: station.object });
+    this._controlPanelAnimation('open');
+  }
+
   render() {
-    // <ControlPanel containerComponent={this.props.containerComponent} settings={settings} onChange={this._onSettingChange} />
     const { viewport, settings } = this.state;
 
     const layer = new ScatterplotLayer({
@@ -69,24 +110,28 @@ export default class App extends Component {
       data: bartStations,
       getPosition: e => e.coordinates,
       getColor: e => [255,140,0],
-      getRadius: e => Math.sqrt(e.exits) * 2,
-      outline: false
+      getRadius: e => {
+        return Math.sqrt(Number(e.entries) + Number(e.exits))
+      },
+      pickable: true,
+      onClick: e => this._stationClicked(e),
+      className: 'station'
     });
 
-    return (
-      <MapGL
-        {...viewport}
-        {...settings}
-        mapStyle="mapbox://styles/mapbox/dark-v9"
-        onViewportChange={this._onViewportChange}
-        mapboxApiAccessToken={MAPBOX_TOKEN} >
-        {/* <BartOverlay
-          viewport={viewport}
-          data={bartStations.coordinates || []}
-        /> */}
-        <DeckGL radiusMinPixels={75} {...viewport} layers={[layer]} />
-      </MapGL>
-    );
-  }
+    return <MapGL
+      {...viewport}
+      {...settings}
+      mapStyle="mapbox://styles/mapbox/dark-v9"
+      onViewportChange={this._onViewportChange}
+      mapboxApiAccessToken={MAPBOX_TOKEN} >
+      {/* <BartOverlay
+        viewport={viewport}
+        data={bartStations.coordinates || []}
+      /> */}
 
+      <DeckGL radiusMinPixels={75} {...viewport} layers={[layer]} />
+
+      {this._renderControlPanel()}
+    </MapGL>
+  }
 }
