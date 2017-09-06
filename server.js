a716cc5336a67369d97ef4f30be33c2833e25c21
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-09-05 17:10:34 -0700

    rrr

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
     app.use(express.static('public'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'index.html'));
+        res.sendFile(path.join(__dirname, '/public/index.html'));
     });
 }
 
