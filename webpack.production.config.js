fe8fb977398863b99299934620253789f191546d
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-09-06 12:52:45 -0700

    finished a few webpack setup changes

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -31,7 +31,7 @@ module.exports = {
     }
   },
   module: {
-    noParse: /(mapbox-gl)\.js$/,
+    noParse: /(mapbox-gl)\.js$/, // necessary or uglify will mess up everything
     loaders
   },
   plugins: [
